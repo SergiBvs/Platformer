267b15a389f628e8663d7a23d69cabd0fbd16ac5
Skfod37 <alex.morillas@epiagranollers.cat>
2020-03-03 23:28:36 +0100

    Camera Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	Transform m_playerPosition;
+	public float yoffset;
+	
+	// Use this for initialization
+	void Start () {
+		m_playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(m_playerPosition.position.x, m_playerPosition.position.y+yoffset,this.transform.position.z);
+		//this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(m_playerPosition.position.x, m_playerPosition.position.y + yoffset, this.transform.position.z), 0.1f);
+	}
+}
