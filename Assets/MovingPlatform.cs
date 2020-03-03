d1d1b4308ca29374af6d17ee268d47dba0bd4e87
Skfod37 <alex.morillas@epiagranollers.cat>
2020-03-03 17:12:02 +0100

    csacsac

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public Transform[] pivots;
+    private Transform currentTarget;
+
+    private int targetInt = 0;
+    int children;
+    int dir = 1;
+
+    // Use this for initialization
+    void Awake () {
+        currentTarget = pivots[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        this.transform.position = Vector3.MoveTowards(this.transform.position, currentTarget.position, 0.2f);
+        if(this.transform.position == currentTarget.position)
+        {
+            if(targetInt == pivots.Length -1)
+            {
+                dir = -1;
+            }
+            else if (targetInt == 0)
+            {
+                dir = 1;
+            }
+
+            targetInt += dir;
+            currentTarget = pivots[targetInt];
+        }
+	}
+}
