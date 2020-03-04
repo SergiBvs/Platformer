c88a604c6f1292d63066664940bdda64b39bd161
Skfod37 <alex.morillas@epiagranollers.cat>
2020-03-05 00:07:30 +0100

    x

---
+++ Assets/ExplosionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ExplosionTime());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator ExplosionTime()
+	{
+		yield return new WaitForSeconds(0.5f);
+		Destroy(this.gameObject);
+	}
+}
--- Assets/PlayerShotScript.cs
+++ Assets/PlayerShotScript.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class PlayerShotScript : MonoBehaviour {
 
-    public float dir;
-    private Player playerScript;
-
-    public float shotSpeed;
+   	public GameObject explosion;
 
 	// Use this for initialization
 	void Start () {
-        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        dir = playerScript.m_lastDirection;
-
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(dir*1.1f, 1) * shotSpeed, ForceMode2D.Impulse);
+		StartCoroutine(ShotExplode());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	IEnumerator ShotExplode()
+	{
+		yield return new WaitForSeconds(1.5f);
+		Instantiate(explosion, transform.position, Quaternion.identity);
+		Destroy(this.gameObject);
 	}
 }
