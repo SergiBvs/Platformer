9a6843828896d3935028b93153a835d37a4dced3
Skfod37 <alex.morillas@epiagranollers.cat>
2020-03-13 11:00:32 +0100

    s

--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -5,29 +5,35 @@ using UnityEngine;
 public class TutorialManager : MonoBehaviour {
 
     public Animator[] keyAnim;
-    bool ledtdone;
-    bool rightdone;
-
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	private bool[] done;
+    public KeyCode[] key;
+	public bool needMore = false;
 	
+	void Start()
+	{
+		done[0] = false;
+		done[1] = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetAxis("Horizontal") < 0 && !rightdone)
-        {
-            keyAnim[0].SetTrigger("OUT");
-            keyAnim[1].SetTrigger("OUT");
-            rightdone = true;
-        }
-        else if (Input.GetAxis("Horizontal") > 0 && !ledtdone)
-        {
-            keyAnim[2].SetTrigger("OUT");
-            keyAnim[3].SetTrigger("OUT");
-            ledtdone = true;
-        }
+		
+		if (Input.GetKeyDown(key[0]) || Input.GetKeyDown(key[1]) && !done[0])
+		{
+			keyAnim[0].SetTrigger("OUT");
+			keyAnim[1].SetTrigger("OUT");
+			done[0] = true;
+		}
+
+		if (needMore)
+		{
+			if (Input.GetKeyDown(key[2]) || Input.GetKeyDown(key[3]) && !done[1])
+			{
+				keyAnim[2].SetTrigger("OUT");
+				keyAnim[3].SetTrigger("OUT");
+				done[1] = true;
+			}
+		}
+		
     }
 }
