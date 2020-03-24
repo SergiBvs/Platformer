a2cc257e95502f4fd7faa261ed236bf8e07bae2d
Skfod37 <alex.morillas@epiagranollers.cat>
2020-03-24 16:07:18 +0100

    luces y tal

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, player.position.y, player.position.z-0.3f), 0.8f);
+	}
+}
