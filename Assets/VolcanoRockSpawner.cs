49941bd89a94e5de841e6b01f9267dbc53c9613a
Skfod37 <alex.morillas@epiagranollers.cat>
2020-04-04 17:33:23 +0200

    vgkj

---
+++ Assets/VolcanoRockSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoRockSpawner : MonoBehaviour {
+
+	float spawnCD = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		spawnCD -= Time.deltaTime;
+		if(spawnCD <= 0)
+		{
+			spawnCD = Random.Range(1f, 3.5f);
+			Instantiate(Resources.Load("VolcanoRock"), new Vector2(transform.position.x + Random.Range(-7f, 7f), transform.position.y), Quaternion.identity);
+		}
+	
+	}
+}
